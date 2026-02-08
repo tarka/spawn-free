d6f11fcd91304be0c7353b540ae4dbfd109201c3
Steve Smith <tarkasteve@gmail.com>
2026-02-08 13:12:09 +1100

    Quick io_uring test

---
+++ tests/uring.rs
@@ -0,0 +1,24 @@
+use io_uring::{IoUring, opcode, types::Timespec};
+
+
+#[test]
+fn test_sleep() {
+    let mut uring = IoUring::new(16).unwrap();
+
+    let wait_for = Timespec::new()
+        .sec(2);
+
+    let op = opcode::Timeout::new(&wait_for)
+        .build()
+        .user_data(2);
+
+    println!("QUEUEING {op:?}");
+    unsafe { uring.submission().push(&op).unwrap()  };
+    println!("SUBMITTING & WAITING");
+    let queued = uring.submit_and_wait(1).unwrap();
+
+    println!("Completed {queued}");
+
+    let resp = uring.completion().next().unwrap();
+    println!("RESULT {resp:?}");
+}
